4261d422c8e1f60ca7e9f73dc17048f6cc912d85
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-11-17 20:57:46 -0500

    update cloudfront invalidation call to invalidate everything

--- publish.sh
+++ publish.sh
@@ -5,29 +5,5 @@ aws configure set preview.cloudfront true
 npm run build
 aws s3 sync public/ s3://docs.smooch.io/ --delete > $OUTPUT
 PATTERN="s3://docs.smooch.io"
-cloudfrond_items=''
-cloudfrond_count=0
-
-while read -r line; do
-    cloudfrond_items+=",'"$line"'"
-    ((cloudfrond_count++))
-
-    base_path=$(echo $line | sed 's/\(.*\)\/.*/\1/')
-    if [[ -z "$base_path" && "$line" = "/index.html" ]]
-    then
-        base_path='/'
-    fi
-    
-    if [[ ! -z "$base_path" ]]
-    then
-        cloudfrond_items+=",'"$base_path"'"
-        ((cloudfrond_count++))
-    fi
-done < <(awk -F $PATTERN '{print $NF}' $OUTPUT)
-
-if [[ "$cloudfrond_count" -gt "0" ]]
-then
-    echo "Invalidating ${cloudfrond_count} items on Cloudfront..."
-    INVALIDATION_BATCH="Paths={Quantity=${cloudfrond_count},Items=[${cloudfrond_items}]},CallerReference=\"build${CIRCLE_BUILD_NUM}\""
-    aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION --invalidation-batch "${INVALIDATION_BATCH}"
-fi
+INVALIDATION_BATCH="Paths={Quantity=1,Items=[/**]},CallerReference=\"build${CIRCLE_BUILD_NUM}\""
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION --invalidation-batch "${INVALIDATION_BATCH}"
