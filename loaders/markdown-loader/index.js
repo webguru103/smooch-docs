36ed31e760623ea944dbd282ebab922879ee664c
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-11-18 16:06:23 -0500

    add remark

--- loaders/markdown-loader/index.js
+++ loaders/markdown-loader/index.js
@@ -1,46 +1,12 @@
 import frontMatter from 'front-matter';
-import markdownIt from 'markdown-it';
-import hljs from 'highlight.js';
 import objectAssign from 'object-assign';
-
-const highlight = (str, lang) => {
-    if ((lang !== null) && hljs.getLanguage(lang)) {
-        try {
-            return hljs.highlight(lang, str).value;
-        }
-        catch (_error) {
-            console.error(_error);
-        }
-    }
-    try {
-        return hljs.highlightAuto(str).value;
-    }
-    catch (_error) {
-        console.error(_error);
-    }
-    return '';
-};
-
-const md = markdownIt({
-    html: true,
-    linkify: true,
-    typographer: true,
-    highlight,
-})
-    .use(require('markdown-it-sub'))
-    .use(require('markdown-it-footnote'))
-    .use(require('markdown-it-deflist'))
-    .use(require('markdown-it-abbr'))
-    .use(require('markdown-it-anchor'))
-    .use(require('markdown-it-attrs'));
 
 module.exports = function(content) {
     this.cacheable();
     const meta = frontMatter(content);
-    const body = md.render(meta.body);
     const result = objectAssign({}, meta.attributes, {
-        body,
+        body: meta.body,
     });
     this.value = result;
-    return `module.exports = ${JSON.stringify(result)}`;
+    return `module.exports = ${JSON.stringify(meta)}`;
 };
--- wrappers/md.js
+++ wrappers/md.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import 'highlight.js/styles/default.css';
 import Helmet from 'react-helmet';
 import { config } from 'config';
-import hljs from 'highlight.js';
+import RemarkLowlight from 'remark-react-lowlight';
+import modifyChildren from 'unist-util-modify-children';
+
+import js from 'highlight.js/lib/languages/javascript';
+import json from 'highlight.js/lib/languages/json';
+import xml from 'highlight.js/lib/languages/xml';
+import bash from 'highlight.js/lib/languages/bash';
+
+import remark from 'remark';
+import reactRenderer from 'remark-react';
+
+import merge from 'deepmerge';
+import sanitizeGhSchema from 'hast-util-sanitize/lib/github.json';
+
 import TwoColumnLayout from '../components/TwoColumnLayout';
 import ThreeColumnLayout from '../components/ThreeColumnLayout';
+
+
+const schema = merge(sanitizeGhSchema, {
+    attributes: {
+        '*': ['className']
+    }
+});
+
+const preFunc = ({children}) => {
+     return React.cloneElement(children[0]);
+}
 
 export default class extends Component {
     static propTypes = {
         router: React.PropTypes.object,
     };
-
-    componentDidMount() {
-        hljs.initHighlightingOnLoad();
-    }
 
     render() {
         const {layout = 'documentation', body, ...data} = this.props.route.page.data;
@@ -30,10 +50,33 @@ export default class extends Component {
         }
 
 
+        const content = remark()
+            .use(reactRenderer, {
+                sanitize: schema,
+                remarkReactComponents: {
+                    code: RemarkLowlight({
+                        json,
+                        js,
+                        javascript: js,
+                        html: xml,
+                        bash,
+                        shell: bash
+                    })
+                }
+            })
+                .use(reactRenderer, {
+                    sanitize: schema,
+                    remarkReactComponents: {
+                        pre: preFunc
+                    }
+                })
+            .process(body)
+            .contents;
+
         return <div className='markdown'>
                    <Helmet title={ `${config.siteTitle} | ${data.title}` } />
                    <Layout {...data}>
-                       <div dangerouslySetInnerHTML={ { __html: body } } />
+                       { content }
                    </Layout>
                </div>;
     }
